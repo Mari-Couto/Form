e2896c926aa1c54e8b771d23174c570a85e8074c
Marina Couto <marinacoutonina@gmail.com>
2023-08-02 11:39:26 -0300

    updateScript

--- script.js
+++ script.js
@@ -1,71 +1,92 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const tel = document.getElementById('tel')
-const senha = document.getElementById('senha')
-const senhaConf = document.getElementById('senhaConfirm')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const tel = document.getElementById('tel');
+const senha = document.getElementById('senha');
+const senhaConf = document.getElementById('senhaConfirm');
 
-form.addEventListener("submit", (event) => {
- event.preventDefault();
- 
- validation();
-})
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  validation();
+});
 
 function validation() {
+  let hasErrors = false;
 
-    const userValue = username.value
-    const emailValue = email.value
-    const telValue = tel.value
-    const senhaValue = senha.value
-    const senhaConfValue = senhaConf.value
+  const userValue = username.value;
+  const emailValue = email.value;
+  const telValue = tel.value;
+  const senhaValue = senha.value;
+  const senhaConfValue = senhaConf.value;
 
-    //username
-    if(userValue === ""){
-        username.classList.add('error')
-        username.classList.remove('success')
+  // username
+  if (userValue === '') {
+    showError(username);
+    hasErrors = true;
+  } else {
+    showSuccess(username);
+  }
 
-    } else {
-        username.classList.add('success')
-        username.classList.remove('error')
-    }
-    
-    //email
-    if(emailValue === ""){
-        email.classList.add('error')
-        email.classList.remove('success')
-    } else {
-        email.classList.add('success')
-        email.classList.remove('error')
-    }
+  // email
+  if (emailValue === '') {
+    showError(email);
+    hasErrors = true;
+  } else {
+    showSuccess(email);
+  }
 
-    //telefone
-    if(telValue === ""){
-        tel.classList.add('error')
-        tel.classList.remove('success')
-    } else {
-        tel.classList.add('success')
-        tel.classList.remove('error')
-    }
+  // telefone
+  if (telValue === '') {
+    showError(tel);
+    hasErrors = true;
+  } else {
+    showSuccess(tel);
+  }
 
-    //senha
-    if(senhaValue === ""){
-        senha.classList.add('error')
-        senha.classList.remove('success')
-    } else {
-        senha.classList.add('success')
-        senha.classList.remove('error')
-    }  
+  // senha
+  if (senhaValue === '') {
+    showError(senha);
+    hasErrors = true;
+  } else {
+    showSuccess(senha);
+  }
 
-    //senha confirmação
-    if(senhaConfValue === ""){
-        senhaConf.classList.add('error')
-        senhaConf.classList.remove('success')
-    } else {
-        senhaConf.classList.add('success')
-        senhaConf.classList.remove('error')
-    }
-  
+  // senha confirmação
+  if (senhaConfValue === '') {
+    showError(senhaConf);
+    hasErrors = true;
+  } else {
+    showSuccess(senhaConf);
+  }
 
+  // Impedir o envio do formulário se houver erros
+  if (hasErrors) {
+    return;
+  }
 
-
+  // Enviar o formulário se não houver erros
+  form.submit();
 }
+
+// Função para tratar o estado de erro do campo
+function showError(input) {
+    input.classList.add('error');
+    input.classList.remove('success');
+    const errorMessage = input.nextElementSibling;
+    errorMessage.style.visibility = 'visible';
+    errorMessage.style.color = 'red';
+    const iconExclamation = input.parentElement.querySelector('.icon-exclamation');
+    iconExclamation.style.visibility = 'visible';
+  }
+
+// Função para tratar o estado de sucesso do campo
+function showSuccess(input) {
+    input.classList.add('success');
+    input.classList.remove('error');
+    const errorMessage = input.nextElementSibling;
+    errorMessage.style.visibility = 'hidden';
+    errorMessage.style.color = 'green';
+    const iconCheck = input.parentElement.querySelector('.icon-check');
+    iconCheck.style.visibility = 'visible';
+  }
